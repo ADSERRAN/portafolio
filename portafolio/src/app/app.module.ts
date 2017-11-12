3298edbcda651d1bd59212d16ff1a7c17f7cc909
Unknown <asgadrian@hotmail.com>
2017-11-12 23:34:22 +0100

    Add rutas

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -3,21 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Rutas
+import { app_routing } from "./app.routes";
+
+// Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { ItemComponent } from './components/item/item.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    app_routing,
     HttpModule
   ],
   providers: [],
